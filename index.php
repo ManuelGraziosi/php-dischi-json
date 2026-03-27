3bb5737c989cb77bd291cdbc5d5a913f397d464e
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-03-27 12:46:08 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <title>EX - PHP Dischi</title>
+</head>
+
+<body>
+    <h1>Benvenuto nell'elenco dei disci</h1>
+</body>
+
+</html>
