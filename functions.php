9625218f4899a8b3362f117024cab776d7205d6b
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-03-27 23:05:50 +0100

    feat: layout implementation with data readed form json

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php
+
+$disks = [
+    [
+        "titolo" => "The Dark Side of the Moon",
+        "artista" => "Pink Floyd",
+        "cover_url" => "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+        "anno" => 1973,
+        "genere" => "Progressive Rock"
+    ],
+    [
+        "titolo" => "Thriller",
+        "artista" => "Michael Jackson",
+        "cover_url" => "https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png",
+        "anno" => 1982,
+        "genere" => "Pop"
+    ],
+    [
+        "titolo" => "Abbey Road",
+        "artista" => "The Beatles",
+        "cover_url" => "https://upload.wikimedia.org/wikipedia/en/4/42/Beatles_-_Abbey_Road.jpg",
+        "anno" => 1969,
+        "genere" => "Rock"
+    ],
+    [
+        "titolo" => "Nevermind",
+        "artista" => "Nirvana",
+        "cover_url" => "https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg",
+        "anno" => 1991,
+        "genere" => "Grunge"
+    ],
+    [
+        "titolo" => "Random Access Memories",
+        "artista" => "Daft Punk",
+        "cover_url" => "https://upload.wikimedia.org/wikipedia/en/a/a7/Random_Access_Memories.jpg",
+        "anno" => 2013,
+        "genere" => "Electronic"
+    ]
+];
+
+//var_dump($disks);
+
+$text_disks = json_encode($disks);
+
+echo $text_disks;
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+$file_string = file_get_contents("disks.json");
+
+$file_decoded = json_decode($file_string, true);
+
+//var_dump($file_decoded);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,11 +14,49 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+          crossorigin="anonymous">
+
     <title>EX - PHP Dischi</title>
 </head>
 
 <body>
-    <h1>Benvenuto nell'elenco dei disci</h1>
+    <div class="container">
+
+        <header>
+            NAV
+            <hr>
+        </header>
+        <section>
+            <h1>Benvenuto nell'elenco dei disci</h1>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
+                <?php
+                foreach ($file_decoded as $disk) {
+                    ?>
+                    <div class="col">
+                        <div class="card">
+                            <img src="<?php echo $disk["cover_url"] ?>"
+                                 class="card-img-top"
+                                 alt="<?php echo $disk["title"] ?>">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $disk["title"] ?></h5>
+                                <p class="card-text">Author: <?php echo $disk["artist"] ?></p>
+                                <p class="card-text">Year: <?php echo $disk["year"] ?></p>
+                                <p class="card-text">Genre: <?php echo $disk["genre"] ?></p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+                ?>
+            </div>
+
+        </section>
+    </div>
+
 </body>
 
 </html>
